3fa76e0379d70003a1e0f76a63b529c72da10d0b
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-07-10 16:28:33 -0300

    loading feito

--- app/src/main/java/com/example/gatinhosfofosaplicativotop/Loading.java
+++ app/src/main/java/com/example/gatinhosfofosaplicativotop/Loading.java
@@ -1,14 +1,29 @@
 package com.example.gatinhosfofosaplicativotop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Loading extends AppCompatActivity {
+    //definindo nomes
+    ImageView gatito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading);
+
+        gatito = findViewById(R.id.gatito);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 5000);
     }
 }
