551f6c0341235f6a88551139e337256b91790434
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-07-19 20:42:06 -0300

    criando layouts

---
+++ app/src/main/java/com/example/gatinhosfofosaplicativotop/Geolocation.java
@@ -0,0 +1,13 @@
+package com.example.gatinhosfofosaplicativotop;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Geolocation extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.geolocation);
+    }
+}
